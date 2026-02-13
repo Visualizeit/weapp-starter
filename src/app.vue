9a0909170ca202447d07892dabde9fd67e695540
Visualizeit <17688172112@163.com>
2026-02-13 20:35:37 +0800

    feat: implement auto-routing feature with dynamic page imports

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+  import routes from "weapp-vite/auto-routes";
+
   defineAppJson({
-    pages: ["pages/index/index"],
+    pages: Array.from(routes.pages),
     window: {
       navigationBarTitleText: "Weapp Starter",
       navigationBarBackgroundColor: "#0969FF",
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "weapp-vite/config";
 export default defineConfig(() => ({
   weapp: {
     srcRoot: "src",
+    autoRoutes: true,
   },
   plugins: [
     UnifiedViteWeappTailwindcssPlugin({
